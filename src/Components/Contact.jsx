cee2df29065631b56d1f563a2843c4d182c66e51
bhushan-ai <bhushaningole2004@gmail.com>
2025-09-09 00:55:00 +0530

    x added

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -13,6 +13,25 @@ text-white dark:text-gray-500 "
         </h2>
         <div className="flex justify-center space-x-4 mb-5">
           <a
+            href="https://x.com/bhushann_ai"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-200 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 transition duration-300 ease-in-out transform hover:scale-105"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="24"
+              height="24"
+              viewBox="0 0 30 30"
+              fill="white"
+            >
+              <path d="M26.37,26l-8.795-12.822l0.015,0.012L25.52,4h-2.65l-6.46,7.48L11.28,4H4.33l8.211,11.971L12.54,15.97L3.88,26h2.65 l7.182-8.322L19.42,26H26.37z M10.23,6l12.34,18h-2.1L8.12,6H10.23z"></path>
+            </svg>
+            <span className="sr-only">X</span>
+          </a>
+          <a
             href="https://github.com/bhushan-ai"
             target="_blank"
             rel="noopener noreferrer"
