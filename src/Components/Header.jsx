5014d42dbb54beb716b53543a883dc24a5975f0e
bhushan-ai <bhushaningole2004@gmail.com>
2025-09-11 18:14:04 +0530

    font updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { ShimmerButton } from "./magicui/shimmer-button";
+import { WordRotate } from "./magicui/word-rotate";
 
 function Header() {
   return (
     <div id="/" className="p-2  m-15 ">
-      <h1 className="text-3xl md:text-5xl text-center font-bold  text-gray-800 dark:text-gray-300 ">
-        Hi I'm Bhushan
-      </h1>
-
-      <p className="text-lg md:text-2xl font-semibold text-gray-500 dark:text-gray-300 text-center mt-5">
-        MERN & GenAi Developer | Building Al Agents & RAG-powered Applications
-      </p>
+      <WordRotate
+        duration={3000}
+        className="text-3xl md:text-5xl text-center font-bold  text-gray-800 dark:text-gray-300 "
+        words={["I'm bhushann", "MERN Dev", "GenAI Dev ", "Backend Dev "]}
+      />
+      <div className="flex items-center justify-center">
+        {" "}
+        <p className="text-lg md:text-2xl font-semibold text-gray-500 dark:text-gray-300 text-center mt-5 font-chirp">
+          I Build Backends | AI Agents & RAG-powered Applications
+        </p>
+      </div>
       <div className="items-center flex justify-center mt-6">
         <ShimmerButton
+          className="p-0 m-0"
           onClick={() =>
             document
               .getElementById("contact")
